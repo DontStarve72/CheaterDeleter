90f7b02e09501e059a9e741bd554379d532b2d07
DontStarve72 <helinos67@protonmail.com>
2022-12-23 12:55:27 -0800

    Deal damage even if player is using nofall

--- src/main/java/io/github/coolmineman/cheaterdeleter/mixin/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/io/github/coolmineman/cheaterdeleter/mixin/ServerPlayNetworkHandlerMixin.java
@@ -1,8 +1,13 @@
 package io.github.coolmineman.cheaterdeleter.mixin;
 
+import io.github.coolmineman.cheaterdeleter.util.CollisionUtil;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.math.Box;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import io.github.coolmineman.cheaterdeleter.events.PlayerInteractBlockCallback;
@@ -14,14 +19,36 @@ import net.minecraft.util.math.BlockPos;
 
 @Mixin(ServerPlayNetworkHandler.class)
 public class ServerPlayNetworkHandlerMixin {
+    @Shadow public ServerPlayerEntity player;
+    private boolean isTouching;
+
     @Inject(method = "onPlayerInteractBlock", at = @At("HEAD"), cancellable = true)
     public void onPlayerInteractBlock(PlayerInteractBlockC2SPacket packet, CallbackInfo cb) {
-        CDPlayer player = CDPlayer.of(((ServerPlayNetworkHandler)(Object)this).player);
+        CDPlayer cdPlayer = CDPlayer.of(player);
         BlockPos pos = packet.getBlockHitResult().getBlockPos();
-        if (!PlayerInteractBlockCallback.EVENT.invoker().onPlayerInteractBlock(player, packet)) {
-            player.getNetworkHandler().sendPacket(new BlockUpdateS2CPacket(player.getWorld(), pos));
-            player.getNetworkHandler().sendPacket(new BlockUpdateS2CPacket(player.getWorld(), pos.offset(packet.getBlockHitResult().getSide())));
+        if (!PlayerInteractBlockCallback.EVENT.invoker().onPlayerInteractBlock(cdPlayer, packet)) {
+            cdPlayer.getNetworkHandler().sendPacket(new BlockUpdateS2CPacket(cdPlayer.getWorld(), pos));
+            cdPlayer.getNetworkHandler().sendPacket(new BlockUpdateS2CPacket(cdPlayer.getWorld(), pos.offset(packet.getBlockHitResult().getSide())));
             cb.cancel();
         } 
+    }
+
+    // Change all vanilla fall damage logic to use our manual onGround check
+    // No reason to trust the client when dealing fall damage
+    @Inject(method = "onPlayerMove", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;updatePositionAndAngles(DDDFF)V", ordinal = 1, shift = At.Shift.AFTER))
+    private void testOnGround(CallbackInfo info) {
+        CDPlayer cdPlayer = CDPlayer.of(player);
+        Box playerBox = cdPlayer.getBoxForPosition(cdPlayer.getPacketX(), cdPlayer.getPacketY(), cdPlayer.getPacketZ()).expand(0.4);
+        isTouching = CollisionUtil.isTouching(cdPlayer, playerBox, cdPlayer.getWorld(), CollisionUtil.touchingRigidTopPredicates(playerBox));
+    }
+
+    @ModifyArg(method = "onPlayerMove", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;handleFall(DZ)V"), index = 1)
+    private boolean handleFall(boolean packetOnGround) {
+        return isTouching;
+    }
+
+    @ModifyArg(method = "onPlayerMove", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;setOnGround(Z)V"), index = 0)
+    private boolean setOnGround(boolean packetOnGround) {
+        return isTouching;
     }
 }
